bf8dc58f20f6ad9c6a69bc868a4acac5ec118469
Melia Syhada <meliasyuhada5@gmail.com>
2022-04-15 11:10:42 +0700

    Intent Example 2

---
+++ app/src/main/java/id/com/diaryme/meliasepti/intentexample2/SecondActivity.kt
@@ -0,0 +1,17 @@
+package id.com.diaryme.meliasepti.intentexample2
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val m_data = intent.getStringExtra("main_activity_data")
+        textView.setText(m_data)
+
+    }
+}
